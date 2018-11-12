a177ebf87f9de9324cc20f7a66e02e2ddac1206a
Jon <43318682+JonKaleidoscope@users.noreply.github.com>
2018-11-11 20:51:51 -0500

    Updating `LinuxMain` tests

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -3,5 +3,7 @@ import XCTest
 @testable import ApplicationTests
 
 XCTMain([
+    testCase(PathGeneratorTests.allTests),
     testCase(RouteTests.allTests),
+    testCase(StringUtils_Tests.allTests),
     ])
